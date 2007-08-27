nikolasco/zotero-oac

commit 0591c62068d0bc3e4b58e39720ab96fd2f123490

The fulltext index wasn't actually being rebuilt correctly since the addition of the INSERT INTO ... SELECT FROM workaround -- fulltextItems has to be populated after the rebuild of fulltextItemWords
git-svn-id: https://www.zotero.org/svn/extension/branches/1.0@1661 45803e0f-1f0d-0410-b1ad-df509fcb3597

chrome/content/zotero/xpcom/schema.js
@@ -151,6 +151,10 @@ Zotero.Schema = new function(){
 					}
 					insertStatement.reset();
 					Zotero.DB.commitTransaction();
+					
+					Zotero.DB.beginTransaction();
+					Zotero.DB.query("INSERT INTO fulltextItems SELECT DISTINCT itemID, 1 FROM fulltextItemWords");
+					Zotero.DB.commitTransaction();
 				}
 				catch (e) {
 					Zotero.debug(e);
@@ -1164,7 +1168,6 @@ Zotero.Schema = new function(){
 					
 					Zotero.DB.query("DROP TABLE fulltextItems");
 					Zotero.DB.query("CREATE TABLE fulltextItems (\n    itemID INT,\n    version INT,\n    PRIMARY KEY (itemID),\n    FOREIGN KEY (itemID) REFERENCES items(itemID)\n);");
-					Zotero.DB.query("INSERT INTO fulltextItems SELECT DISTINCT itemID, 1 FROM fulltextItemWords");
 				}
 				
 				if (i==36) {

commit 3026c2837a3763d2747227669c920156d938ca2c

Fix SQL error caused by r1661
git-svn-id: https://www.zotero.org/svn/extension/branches/1.0@1662 45803e0f-1f0d-0410-b1ad-df509fcb3597

chrome/content/zotero/xpcom/schema.js
@@ -153,7 +153,7 @@ Zotero.Schema = new function(){
 					Zotero.DB.commitTransaction();
 					
 					Zotero.DB.beginTransaction();
-					Zotero.DB.query("INSERT INTO fulltextItems SELECT DISTINCT itemID, 1 FROM fulltextItemWords");
+					Zotero.DB.query("INSERT INTO fulltextItems SELECT DISTINCT itemID, 1, NULL, NULL, NULL, NULL FROM fulltextItemWords");
 					Zotero.DB.commitTransaction();
 				}
 				catch (e) {