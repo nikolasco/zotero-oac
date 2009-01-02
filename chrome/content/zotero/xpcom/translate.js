nikolasco/zotero-oac

commit d9068404c8cb95e32a4c73d10e6df3265e0e1a51

Fix export error (broken by r3603)
git-svn-id: https://www.zotero.org/svn/extension/trunk@3652 45803e0f-1f0d-0410-b1ad-df509fcb3597

chrome/content/zotero/xpcom/translate.js
@@ -2147,11 +2147,12 @@ Zotero.Translate.prototype._exportToArray = function(returnItem) {
 	// preserve notes
 	if(returnItemArray.note) returnItemArray.uniqueFields.note = returnItemArray.note;
 	
-	// TODO: Change tag.tag references in scrapers.sql to tag.name
+	// TODO: Change tag.tag references in translators to tag.name
 	// once translators are 1.5-only
+	// TODO: Preserve tag type?
 	if (returnItemArray.tags) {
 		for (var i in returnItemArray.tags) {
-			returnItemArray.tags[i].tag = returnItemArray.tags[i].name;
+			returnItemArray.tags[i].tag = returnItemArray.tags[i].fields.name;
 		}
 	}
 	
@@ -2168,7 +2169,8 @@ Zotero.Translate.prototype._exportGetCollection = function() {
 	
 	if(this._collectionsLeft && this._collectionsLeft.length != 0) {
 		var returnItem = this._collectionsLeft.shift();
-		return returnItem.toArray();
+		var obj = returnItem.serialize();
+		obj.name = obj.fields.name;
 	}
 }
 

commit 7387397dc3571e8ff71c53974e21317411d374bc

Fixes #1276, RDF export loses collection info (broken by me in r3652)
git-svn-id: https://www.zotero.org/svn/extension/trunk@3962 45803e0f-1f0d-0410-b1ad-df509fcb3597

chrome/content/zotero/xpcom/translate.js
@@ -2177,6 +2177,7 @@ Zotero.Translate.prototype._exportGetCollection = function() {
 		var returnItem = this._collectionsLeft.shift();
 		var obj = returnItem.serialize();
 		obj.name = obj.fields.name;
+		return obj;
 	}
 }
 