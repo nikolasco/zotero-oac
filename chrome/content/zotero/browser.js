nikolasco/zotero-oac

commit 074015f41bffc8ef0753f49a6f794b6f82052e68

fix frame handling
git-svn-id: https://www.zotero.org/svn/extension/branches/1.0@3987 45803e0f-1f0d-0410-b1ad-df509fcb3597

chrome/content/zotero/browser.js
@@ -364,22 +364,31 @@ var Zotero_Browser = new function() {
 	 * called to unregister Zotero icon, etc.
 	 */
 	function contentHide(event) {
-		if(event.originalTarget instanceof HTMLDocument && !event.originalTarget.defaultView.frameElement) {
+		if(event.originalTarget instanceof HTMLDocument) {
+			// Get root document if this is a frameset
 			var doc = event.originalTarget;
+			var rootDoc = doc;
+			while(rootDoc.defaultView.frameElement) {
+				rootDoc = rootDoc.defaultView.frameElement.ownerDocument;
+			}
 			
 			// Figure out what browser this contentDocument is associated with
 			var browser;
 			for(var i=0; i<this.tabbrowser.browsers.length; i++) {
-				if(doc == this.tabbrowser.browsers[i].contentDocument) {
+				if(rootDoc == this.tabbrowser.browsers[i].contentDocument) {
 					browser = this.tabbrowser.browsers[i];
 					break;
 				}
 			}
 			
-			// clear data object
 			var tab = _getTabObject(browser);
 			if(!tab) return;
-			tab.clear();
+			if(doc == tab.page.document || doc == rootDoc) {
+				// clear translator only if the page on which the pagehide event was called is
+				// either the page to which the translator corresponded, or the root document
+				// (the second check is probably paranoid, but won't hurt)
+				tab.clear();
+			}
 			
 			// update status
 			if(this.tabbrowser.selectedBrowser == browser) {
@@ -587,14 +596,11 @@ Zotero_Browser.Tab.prototype.detectTranslators = function(rootDoc, doc) {
 	// if there's already a scrapable page in the browser window, and it's
 	// still there, ensure it is actually part of the page, then return
 	if(this.page.translators && this.page.translators.length && this.page.document.location) {
-		if(this._searchFrames(rootDoc, this.page.document)) {
-			return;
-		} else {
-			this.page.document = null;
-		}
+		if(this._searchFrames(rootDoc, this.page.document)) return;
+		this.clear();
 	}
 	
-	if(doc instanceof HTMLDocument) {
+	if(doc instanceof HTMLDocument && doc.documentURI.substr(0, 6) != "about:") {
 		// get translators
 		var me = this;
 		
@@ -602,7 +608,7 @@ Zotero_Browser.Tab.prototype.detectTranslators = function(rootDoc, doc) {
 		translate.setDocument(doc);
 		translate.setHandler("translators", function(obj, item) { me._translatorsAvailable(obj, item) });
 		translate.getTranslators();
-	} else if(doc.documentURI.length > 7 && doc.documentURI.substr(0, 7) == "file://") {
+	} else if(doc.documentURI.substr(0, 7) == "file://") {
 		this._attemptLocalFileImport(doc);
 	}
 }

commit 3be51d5de31baba49626e1a8bca8757e432df66f

fix iframe bug introduced by r3987
git-svn-id: https://www.zotero.org/svn/extension/branches/1.0@3990 45803e0f-1f0d-0410-b1ad-df509fcb3597

chrome/content/zotero/browser.js
@@ -618,6 +618,7 @@ Zotero_Browser.Tab.prototype.detectTranslators = function(rootDoc, doc) {
  * searches for a document in all of the frames of a given document
  */
 Zotero_Browser.Tab.prototype._searchFrames = function(rootDoc, searchDoc) {
+	if(rootDoc == searchDoc) return true;
 	var frames = rootDoc.getElementsByTagName("frame");
 	for each(var frame in frames) {
 		if(frame.contentDocument &&