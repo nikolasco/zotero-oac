2275111643129c761802b1c647b5aef1797f290f
dreside <dreside@192.168.0.101>
2010-04-11 21:41:49 -0400

    committing

---
+++ chrome/content/zotero-content/other.js
@@ -0,0 +1,32 @@
+$("#mark-moment").click(function () {markNow();});
+var mr = $("#mark-range");
+mr.click(function () {markStartEnd();});
+mr.toggle(
+  function () {mr.text("...End range");},
+  function () {mr.text("Start a range...");}
+);
+
+
+function changeNote(content, isRange, num){
+	txt = content.firstChild.nodeValue;
+	var cpn = $(content.parentNode);
+	$(content.parentNode).html("<span><input type='text' onblur='recordNote(this,"+isRange+","+num+")' value='"+txt+"' size='20'/></span>");
+	cpn[0].firstChild.firstChild.select();
+
+
+}
+function recordNote(content, isRange, num ){
+	txt = content.value;
+	var cpn = $(content.parentNode);
+	cpn.html("<span onmousedown='changeNote(this,"+isRange+","+num+")'>"+txt+"</span>");
+	cpn.trigger('saveNoteEvent',[isRange,num,txt]);
+}
+function deleteNote(content,isRange,num){
+	// TODO: We'll want a prompt to ensure intent eventually - DLR
+	var row = $(content.parentNode.parentNode);
+	row.trigger('deleteNoteEvent',[isRange,num]);
+	row[0].parentNode.removeChild(row[0]);
+	
+}
+
+build([]);
