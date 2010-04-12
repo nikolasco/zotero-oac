acab9e4890151abbc4f49d14ad1144f0a459c608
Doug Reside <dreside@doug-resides-macbook-2.local>
2010-04-12 11:08:34 -0400

    Adding function for changing save button

--- chrome/content/zotero-content/other.js
+++ chrome/content/zotero-content/other.js
@@ -13,12 +13,21 @@ function changeNote(content, isRange, num){
 	$(content.parentNode).html("<span><input type='text' onblur='recordNote(this,"+isRange+","+num+")' value='"+txt+"' size='20'/></span>");
 	cpn[0].firstChild.firstChild.select();
 
+    $(cpn[0].nextSibling.firstChild).html("<img alt='edit' src='chrome://zotero-content/skin/images/annotate-audio-save.png'>");
+
+
 
 }
 function recordNote(content, isRange, num ){
 	txt = content.value;
 	var cpn = $(content.parentNode);
 	cpn.html("<span onmousedown='changeNote(this,"+isRange+","+num+")'>"+txt+"</span>");
+
+
+  
+    $(cpn[0].parentNode.nextSibling.firstChild).html("<img alt='edit' src='chrome://zotero-content/skin/annotate-audio-edit.png'>");
+   
+    
 	cpn.trigger('saveNoteEvent',[isRange,num,txt]);
 }
 function deleteNote(content,isRange,num){
