b29330d84758d3937884c998d265324e4c919a69
Nikolas Coukouma <atrus@atrus.org>
2010-04-22 15:59:46 -0400

    make emacs hep us with Zotero's style guidelines

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 4)
+         (fill-column . 80)))
+ (c-mode . ((c-file-style . "k&r")))
+ (nxml-mode . ((nxml-child-indent . 4)
+			   (nxml-outline-child-indent . 4))))
